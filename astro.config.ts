19ab1dc97f87d743a6731d8fb606ac3c246f4fa3
Olivier Garcia <olivier@lol.cat>
2025-12-18 00:49:53 +0100

    chore(config): update astro site

--- astro.config.ts
+++ astro.config.ts
@@ -40,7 +40,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thought-lite.vercel.app",
+  site: "https://foudubus.fr",
   trailingSlash: "never",
 
   i18n: {
